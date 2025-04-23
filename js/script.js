849e28485ed6def6d6125778bcccd2d54aa576dd
SemiRoman <semionovroman9@gmail.com>
2025-04-23 11:40:44 +0300

    A treia versiune a proiectului

--- js/script.js
+++ js/script.js
@@ -1,13 +1,32 @@
-const blur = document.querySelector(".mouse-blur");
+window.addEventListener("scroll", function () {
+  const img = document.querySelector(".scrolling-img img");
+  const section = document.querySelector("#about-us");
+  const rect = section.getBoundingClientRect();
 
-document.addEventListener("mousemove", (e) => {
-  blur.style.left = `${e.clientX - 75}px`;
-  blur.style.top = `${e.clientY - 75}px`;
-
-  const hue = (e.clientX / window.innerWidth) * 360;
-  const hue2 = (e.clientY / window.innerHeight) * 360;
-
-  blur.style.background = `radial-gradient(circle, hsl(${hue}, 100%, 70%), hsl(${hue2}, 100%, 75%), hsl(${
-    (hue + hue2) / 2
-  }, 100%, 60%))`;
+  if (rect.top <= window.innerHeight && rect.bottom >= 0) {
+    const scrollSpeed = 0.4;
+    const offset = rect.top * scrollSpeed;
+    img.style.transform = `translateY(${offset}px)`;
+  }
 });
+
+function initMap() {
+  var mapOptions = {
+    zoom: 10,
+    center: { lat: 47.0105, lng: 28.8638 }, // Default location
+  };
+  var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+  var locations = [
+    { title: "Location 1", lat: 40.7128, lng: -74.006 },
+    { title: "Location 2", lat: 40.73061, lng: -73.935242 },
+  ];
+
+  locations.forEach(function (location) {
+    var marker = new google.maps.Marker({
+      position: { lat: location.lat, lng: location.lng },
+      map: map,
+      title: location.title,
+    });
+  });
+}
