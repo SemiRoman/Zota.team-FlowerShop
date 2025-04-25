0bddc9c3fd997a8a1c488e5d0ed709d900b71549
SemiRoman <semionovroman9@gmail.com>
2025-04-25 18:46:54 +0300

    Versiunea a patra

--- js/script.js
+++ js/script.js
@@ -30,3 +30,81 @@ function initMap() {
     });
   });
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  const aboutUs = document.getElementById("about-us");
+  const video = document.getElementById("behindScenesVideo");
+
+  video.addEventListener("ended", function () {
+    video.currentTime = 0;
+    video.play();
+  });
+
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          video.play();
+        }
+      });
+    },
+    {
+      threshold: 0.5,
+    }
+  );
+
+  observer.observe(aboutUs);
+});
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   const searchInput = document.getElementById("search");
+//   const categoryFilters = document.querySelectorAll('input[name="category"]');
+//   const priceRange = document.getElementById("price-range");
+//   const priceValue = document.getElementById("price-value");
+
+//   priceRange.addEventListener("input", () => {
+//     priceValue.textContent = `$${priceRange.value}`;
+//     filterProducts();
+//   });
+
+//   function filterProducts() {
+//     const searchTerm = searchInput.value.toLowerCase();
+//     const selectedCategories = Array.from(categoryFilters)
+//       .filter((checkbox) => checkbox.checked)
+//       .map((checkbox) => checkbox.parentNode.textContent.trim().toLowerCase());
+//     const priceLimit = parseInt(priceRange.value, 10);
+
+//     const productItems = document.querySelectorAll(".product-item");
+
+//     productItems.forEach((item) => {
+//       const name = item.querySelector("h3").textContent.toLowerCase();
+//       const price = parseFloat(
+//         item.querySelector(".product-price").textContent.replace("$", "")
+//       );
+
+//       const matchesSearch = name.includes(searchTerm);
+//       const matchesCategory =
+//         selectedCategories.length === 0 ||
+//         selectedCategories.some((category) =>
+//           item
+//             .querySelector(".product-category")
+//             .textContent.toLowerCase()
+//             .includes(category)
+//         );
+//       const matchesPrice = price <= priceLimit;
+
+//       if (matchesSearch && matchesCategory && matchesPrice) {
+//         item.style.display = "block";
+//       } else {
+//         item.style.display = "none";
+//       }
+//     });
+//   }
+
+//   searchInput.addEventListener("input", filterProducts);
+//   categoryFilters.forEach((checkbox) =>
+//     checkbox.addEventListener("change", filterProducts)
+//   );
+
+//   filterProducts();
+// });
